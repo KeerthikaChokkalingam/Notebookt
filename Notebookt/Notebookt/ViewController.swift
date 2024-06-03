85eaa55e6aa78e2c284216e01da313059631db24
Keerthika Chokkalingam <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2024-06-03 16:50:54 +0530

    commit with initial commit

---
+++ Notebookt/Notebookt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notebookt
+//
+//  Created by Keerthika on 03/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
